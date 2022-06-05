99421910eb90d55c72cc7955dc152b1cb460bcd8
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2022-06-05 20:02:43 +1000

    Started the scaffold of the site

---
+++ src/components/Viewer.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import styles from '../styles/viewer.module.css';
+
+type ViewerProps = {
+  text: string
+};
+
+const Viewer = ({ text }: ViewerProps) => (
+  <div className={styles.viewer}>
+    {text}
+  </div>
+);
+
+export default Viewer;
