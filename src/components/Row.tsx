e942a96453c57191996f569fed7331fb37d2bbf9
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2022-10-21 12:23:11 +1100

    The position of the item in the table didn't match the key sometimes (#17)
    Now the values in the column should definitely match the key

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -2,16 +2,24 @@ import styles from 'styles/table.module.css';
 
 type RowProps = {
   item: Record<string, unknown>;
+  keys: string[];
 };
 
-const Row = ({ item }: RowProps) => {
+const Row = ({ item, keys }: RowProps) => {
   const values: React.ReactNode[] = [];
-  for (const key of Object.keys(item)) {
-    values.push(
-      <td key={key} className={styles.row}>
-        {String(item[key])}
-      </td>
-    );
+  for (const key of keys) {
+    if (key in item) {
+      values.push(
+        <td key={key} className={styles.row}>
+          {String(item[key])}
+        </td>
+      );
+    } else {
+      values.push(
+        <td key={key} className={styles.row} />
+      );
+    }
+    
   }
   return <tr>{values}</tr>;
 };
